af9f39e7095fc97c6d6facd7f582d48d81b4e1da
FUNCIONAL\rodrigo.caraca <rodrigo.caraca@funcionalcorp.com.br>
2022-01-12 00:03:47 -0300

    Implementação da estrutura e carga de dados de SalarioMinimo

---
+++ ControlePessoal_HorasTrabalhadas_Holerite/ControlePessoal.Domain/Entities/SalarioMinimo.cs
@@ -0,0 +1,52 @@
+﻿using Core.CQRS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlePessoal.Domain.Entities
+{
+    public class SalarioMinimo : Entity
+    {
+        // construtor usado apenas para a carga inicial de dados
+        public SalarioMinimo(int idSalarioMinimo, DateTime dataVigenciaInicial, double valor)
+        {
+            IdSalarioMinimo = idSalarioMinimo;
+            DataVigenciaInicial = dataVigenciaInicial;
+            Valor = valor;
+        }
+
+        public SalarioMinimo(DateTime dataVigenciaInicial, double valor)
+        {
+            DataVigenciaInicial = dataVigenciaInicial;
+            Valor = valor;
+        }
+
+        public int IdSalarioMinimo { get; private set; }
+        public DateTime DataVigenciaInicial { get; private set; }
+        public double Valor { get; private set; }
+        public DateTime DataHoraInclusao { get; private set; }
+        public DateTime? DataHoraAlteracao { get; private set; }
+
+        public void UpdateDataVigenciaInicial(DateTime dataVigenciaInicial)
+        {
+            DataVigenciaInicial = dataVigenciaInicial;
+        }
+
+        public void UpdateValor(double valor)
+        {
+            Valor = valor;
+        }
+
+        public void UpdateDataHoraInclusao(DateTime dataHoraInclusao)
+        {
+            DataHoraInclusao = dataHoraInclusao;
+        }
+
+        public void UpdateDataHoraAlteracao(DateTime dataHoraAlteracao)
+        {
+            DataHoraAlteracao = dataHoraAlteracao;
+        }
+    }
+}
